761ab95210df097913412a9fd3f4d4a126968c70
[Sebastian Ochal] <[sebastianochal1@gmail.com]>
2025-02-05 13:47:01 +0000

    created functions for clearing the list and deleting individual items

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const deleteAll = document.getElementById("rAll")
 const hili = document.getElementById("hili");
 const taskList = document.getElementById("taskList");
 const inputTask = document.getElementById("inputTask");
+const removeBtn = document.querySelectorAll(".remove")
+let deleteBtn = [];
 let currentTasks = [];
 
 // an object of elements to be created within a list
@@ -24,6 +26,7 @@ function handleAddTask(){
         console.dir(li);
         inputTask.value ="";
         // iterate through an object create elements based on its key then append to aa list item
+        // TODO: seperate into own function, Could be useful
         Object.keys(listItems).forEach(key =>{
             let x = document.createElement(`${listItems[key]}`);
             // change the elements atriibutes based on type
@@ -33,8 +36,11 @@ function handleAddTask(){
                 x.innerText = taskText;
             }else if(key === "remove"){ 
                 x.innerText = "delete";
-                x.classList.add("btn")
-                x.classList.add("remove")}
+                x.classList.add("btn");
+                x.classList.add("remove");
+                deleteBtn.push(x);
+                handleRemoveIvdividual();
+            }
             // append the new element 
             li.append(x);
         })
@@ -43,11 +49,21 @@ function handleAddTask(){
         console.log(`Amount of Tasks: ${currentTasks.length}`);
     }
 }
-function handleRemoveTask(){
+// clear the list
+function handleRemoveAll(){
     for (let i = 0; i < currentTasks.length; i++) {
         currentTasks[i].remove();
-        console.log("Tasks removed")
     }
+    currentTasks = [];
+    console.log(`Amount of Tasks: ${currentTasks.length}`)
 }
+function handleRemoveIvdividual(){
+    deleteBtn.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            alert(`Button ${index + 1} was pressed!`);
+        });
+    });
+}
+
 add.onclick = handleAddTask;
-deleteAll.onclick = handleRemoveTask;
+deleteAll.onclick = handleRemoveAll;
