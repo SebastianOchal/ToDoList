f9957d46b92f0f08f3272a05987b6c93b3a401c6
[Sebastian Ochal] <[sebastianochal1@gmail.com]>
2025-02-06 17:10:44 +0000

    Happy with completing moduel will work on tommorrow

---
+++ createListItem.js
@@ -0,0 +1,57 @@
+
+// an object of elements to be created within a list
+const taskList = document.getElementById("taskList");
+const inputTask = document.getElementById("inputTask");
+const listItems = {check:"input", text:"p", remove:"button"}
+export const itemArrays = {
+    currentTasks: [],
+    taskCheck: [],
+    taskP: [],
+    deleteBtn: [],
+}
+
+
+export function handleAddTask(){
+    // remove empty space
+    const taskText = inputTask.value.trim();
+    // check of there is text to be added
+    if(inputTask.value === "" ){
+        console.log("Input empty no task added");
+    }else{
+        // console.log("New Task Added");
+        // create a new list item and append it to an unorganized list
+        const li = document.createElement("li");
+        li.classList.add("task")
+        taskList.appendChild(li);
+        console.dir(li);
+        inputTask.value ="";
+        addTaskChildren(taskText, li);
+        // add element to array
+        itemArrays.currentTasks.push(li);
+        console.log(`Amount of Tasks: ${itemArrays.currentTasks.length}`);
+    }
+}
+function addTaskChildren(taskText, parent){
+    // iterate through an object create elements based on its key then append to a list item
+        Object.keys(listItems).forEach(key =>{
+            let item = document.createElement(`${listItems[key]}`);
+            // change the elements atriibutes based on type
+            if (key === "check"){
+                item.setAttribute("type", "checkbox");
+                itemArrays.taskCheck.push(item);
+            } else if(key === "text"){
+                item.innerText = taskText;
+                itemArrays.taskP.push(item);
+            }else if(key === "remove"){ 
+                item.innerText = "delete";
+                item.classList.add("btn");
+                item.classList.add("remove");
+                itemArrays.deleteBtn.push(item);
+                item.addEventListener('click', (event)=>{
+                    handleRemoveIvdividual(event);
+                });
+            }
+            // append the new element 
+            parent.append(item);
+        })
+}
--- script.js
+++ script.js
@@ -1,56 +1,11 @@
 const add = document.getElementById("add");
 const deleteAll = document.getElementById("rAll")
 const hili = document.getElementById("hili");
-const taskList = document.getElementById("taskList");
-const inputTask = document.getElementById("inputTask");
-const removeBtn = document.querySelectorAll(".remove")
-let deleteBtn = [];
-let currentTasks = [];
 
-// an object of elements to be created within a list
-const listItems = {check:"input", text:"p", remove:"button"}
+import { handleAddTask, itemArrays } from "./createListItem.js";
 
 
-function handleAddTask(){
-    // remove empty space
-    const taskText = inputTask.value.trim();
-    // check of there is text to be added
-    if(inputTask.value === "" ){
-        console.log("Input empty no task added");
-    }else{
-        // console.log("New Task Added");
-        // create a new list item and append it to an unorganized list
-        const li = document.createElement("li");
-        li.classList.add("task")
-        taskList.appendChild(li);
-        console.dir(li);
-        inputTask.value ="";
-        // iterate through an object create elements based on its key then append to aa list item
-        // TODO: seperate into own function, Could be useful
-        Object.keys(listItems).forEach(key =>{
-            let x = document.createElement(`${listItems[key]}`);
-            // change the elements atriibutes based on type
-            if (key === "check"){
-                x.setAttribute("type", "checkbox");
-            } else if(key === "text"){
-                x.innerText = taskText;
-            }else if(key === "remove"){ 
-                x.innerText = "delete";
-                x.classList.add("btn");
-                x.classList.add("remove");
-                deleteBtn.push(x);
-                x.addEventListener('click', (event)=>{
-                    handleRemoveIvdividual(event);
-                });
-            }
-            // append the new element 
-            li.append(x);
-        })
-        // add element to array
-        currentTasks.push(li);
-        console.log(`Amount of Tasks: ${currentTasks.length}`);
-    }
-}
+
 // clear the list
 function handleRemoveAll(){
     for (let i = 0; i < currentTasks.length; i++) {
@@ -69,9 +24,18 @@ function handleRemoveIvdividual(event){
     deleteBtn.splice(buttonIndex,1);
     console.log(`button pushed: ${buttonIndex} current tasks: ${currentTasks.length}`)
 }
-function handleHighlightIndividual(){
-
+// ! Stoping for the day: 
+// TODO create a delete module (has to loop through itemArrays and remove an element from each)
+// TODO sepperate handel ivdvidual
+// TODO create Highlight and mark complete
+// TODO CSS styling
+function checkIfUpdate(){
+    Object.keys(itemArrays).forEach(key =>{
+        console.log(`Item Container: ${key} items contained: ${itemArrays[key]}`)
+    })
 }
 
 add.onclick = handleAddTask;
 deleteAll.onclick = handleRemoveAll;
+hili.onclick = checkIfUpdate;
+
