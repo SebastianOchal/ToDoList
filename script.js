fed2a80146d5e463b98a33dbe5b2e5bb1ee35bfe
[Sebastian Ochal] <[sebastianochal1@gmail.com]>
2025-02-07 15:01:03 +0000

    Css complete, have to learn how storage works do it persists when closing the tab

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const add = document.getElementById("add");
 const deleteAll = document.getElementById("rAll")
 
-import { handleAddTask, itemArrays } from "./createListItem.js";
+import { handleAddTask} from "./createListItem.js";
 import { handleDeleteChecked } from "./selectCompleted.js";
 
 add.onclick = handleAddTask;
